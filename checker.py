1e904e2ba61ea36abca8b867be7865e336d1ba03
cbster <cbster@protonmail.com>
2020-01-06 20:41:49 +0800

    Major Update - Threading to provide progress, added docstrings and comments, improved legibility, fixed bugs

--- checker.py
+++ checker.py
@@ -1,3 +1,8 @@
+"""
+This program will take the JSON full of mnemonic phrases written by main.py.
+These are then converted to Base58(P2SH) i.e Segwit addresses.
+The Blockcypher library is used to check if there have been past transactions on the accounts.
+"""
 import json
 
 import blockcypher
@@ -5,17 +10,17 @@ from bip32utils import BIP32Key
 from bip32utils import BIP32_HARDEN
 from btclib import bip39
 
-with open("valid_possibilities.out.txt", 'r') as possibilities:
+with open("valid_possibilities.out.txt", 'r') as possibilities:  # Reading in JSON written by main.py
     all_possibilities = json.loads(possibilities.read())
 addresses = {}
-
-print("Checking possibilities...")
+print("Deriving addresses")
 for possibility in all_possibilities:
     str_poss = ' '.join(possibility)
-    seed = bip39.seed_from_mnemonic(str_poss, "")
-    key = BIP32Key.fromEntropy(seed)
-    account_number = 0
-    i = 0
+    seed = bip39.seed_from_mnemonic(str_poss, "")  # Converting mnemonic to BIP39 Seed
+    key = BIP32Key.fromEntropy(seed)  # Converting to BIP32 Root Key
+    account_number = 0  # This variable can be changed to attain a different derivation path
+    i = 0               # This variable can be changed to attain a different derivation path
+    # For the following account derivation, `BIP32_HARDEN` can be simply removed to access unhardened addresses
     addr = key.ChildKey(49 + BIP32_HARDEN) \
         .ChildKey(0 + BIP32_HARDEN) \
         .ChildKey(account_number + BIP32_HARDEN) \
@@ -24,7 +29,9 @@ for possibility in all_possibilities:
         .P2WPKHoP2SHAddress()
     addresses[addr] = str_poss
 
+print(f"{len(addresses)} addresses derived.")
 for address in addresses:
+    print(f"Checking address {address}")
     if blockcypher.get_total_num_transactions(address) > 0:
-        print(f'The correct address is {address} with mnemonic: {addresses[addr]}')
+        print(f'The correct address is {address}\nThe correct mnemonic is: \n{addresses[addr]}')
         quit()
--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-import json
+"""
+This script will take the hints and words from words_hints.py and derive from them a list of valid mnemonics that
+fulfill the following criteria (in order of how they are sorted in this program):
+1. The words in the mnemonic phrase are spelled correctly and exist in the BIP39 dictionary.
+2. The mnemonic phrases are only represented once in the list of possibilities.
+3. The mnemonic phrases do not repeat any words within themselves.
+4. The mnemonic phrase, when converted to entropy, contains a valid checksum according to BIP39 spec.
+"""
 
-from btclib import bip39
-import numpy
 import itertools
+import json
+import time
+from threading import Thread
+
+import numpy
+from btclib import bip39
+
 from words_hints import hints, mnemonics
-import datetime
 
 with open("dictdata/english.txt", 'r') as bip_39_words:
     all_mnemonic_words = bip_39_words.read().split("\n")[:-1]
 
 
 def mnemonic_valid_check(word_list):
+    """Checks that the words from a given list are in the BIP39 dictionary in ../dictdata"""
     for words in word_list:
         if words not in all_mnemonic_words:
             print("Error! Mnemonic words are not valid.")
@@ -19,6 +31,8 @@ def mnemonic_valid_check(word_list):
 
 
 def words_starting_with(word_list):
+    """Creates a dictionary containing a letter as key
+    and how many times a word begins with that letter in the given list as its value"""
     word_chars = {}
     for word in word_list:
         word_first_char = word[0]
@@ -29,16 +43,18 @@ def words_starting_with(word_list):
 
 
 def hints_starting_with(hint_list):
+    """Creates a dictionary containing a letter as key
+    and how many times a hint begins with that letter in the given list as its value"""
     hint_chars = {}
     for hint_char in hint_list:
         if hint_char != "":
             hint_chars[hint_char] = 0
         hint_chars[hint_char] += 1
-    print(hint_chars)
     return hint_chars
 
 
 def construct_list_apc(hint_list, word_list):
+    """Returns a list of lists containing the possible words that would fit into a given slot according to the hints"""
     all_possible_combos = []
     for char in hint_list:
         if len(char) == 1:
@@ -50,45 +66,66 @@ def construct_list_apc(hint_list, word_list):
     return all_possible_combos
 
 
-def num_possible_combos(apc):
-    return [len(combos) for combos in apc]
+def combination_count(list_of_combinations):
+    """Used to calculate the number of possible combinations when words are not repeated within mnemonic phrase"""
+    lst = []
+    char_count = {word[0][0]: len(word) for word in list_of_combinations}
+    for letter in hints:
+        lst.append(char_count[letter])
+        char_count[letter] -= 1
+    return lst
+
+
+def timer():
+    """Run in a thread, provides a representation of progress during the filtering phase."""
+    latest_filter_count = 0
+    filter_start_time = time.time()
+    while is_filtering:
+        time.sleep(5)
+        elapsed_time = time.time() - filter_start_time
+        speed = (filter_count - latest_filter_count) / 5
+        time_remaining = (unfiltered_possible_combos - filter_count) / speed
+        percentage = int((filter_count / unfiltered_possible_combos) * 100)
+        print(f"{len(possibilities_filtered)} found, {percentage}% scanned, {int(round(speed / 1000))}k/s "
+              f"(Elapsed: {int(round(elapsed_time))}s, Remaining: {int(round(time_remaining))}s)")
+        latest_filter_count = filter_count
 
 
 mnemonic_valid_check(mnemonics)
-total_possible_combos = []
 all_possible_combinations = construct_list_apc(hints, mnemonics)
-npc = num_possible_combos(all_possible_combinations)
-for x in npc:
-    if x == 1:
-        total_possible_combos.append(x)
-    elif 1 < x:
-        total_possible_combos.append(x - 1)
-total_possible_combos = numpy.prod(total_possible_combos)
-unfiltered_possible_combos = numpy.prod(num_possible_combos(construct_list_apc(hints, mnemonics)))
+# The below section provides an instant calculation of how many combinations are possible
+unfiltered_possible_combos = numpy.prod([len(combos) for combos in all_possible_combinations])
+total_possible_combos = numpy.prod(combination_count(all_possible_combinations))
 print(f'Total possible combinations (with repetitions): {unfiltered_possible_combos}')
 print(f'Total possible combinations (without repetitions): {total_possible_combos}')
-possibilities = itertools.product(*all_possible_combinations)
-print("Filtering possibilities...")
+# From here, the search for the combinations themselves begins
+possibilities = itertools.product(*all_possible_combinations)  # Produces a cartesian product
+print("\nChecking for unique phrases (no repeated words)...")
+
 possibilities_filtered = []
+is_filtering = True
 filter_count = 0
+
+status = Thread(target=timer)  # Sets the thread that will display filtering progress
+status.start()
 for possibility in possibilities:
-    if len(possibility) == len(set(possibility)):  # sets must have unique items
+    if len(possibility) == len(set(possibility)):  # Sets must have unique items (see point 3 in docstring)
         possibilities_filtered.append(possibility)
     filter_count += 1
+is_filtering = False
+status.join()  # Status thread is no longer required
+print(f"\n{len(possibilities_filtered)} unique phrases found. These will have their checksums validated.")
 
 valid_mnemonics = []
-print("Validating...")
+print("\nValidating...")
 for mnemonic_phrase in possibilities_filtered:
-    # BIP-39 Filtering
-    try:
+    try:  # BIP39 Filtering - checks whether the phrases produce a valid entropy according to BIP39.
         bip39.entropy_from_mnemonic(" ".join(mnemonic_phrase), "en")
         valid_mnemonics.append(mnemonic_phrase)
-        print(f"Found valid BIP39 - ({len(valid_mnemonics)}/{len(possibilities_filtered)})")
-    except ValueError:
+    except ValueError:  # Entropy was invalid
         pass
 
-# Write BIP-39 valid mnemonics
-with open("valid_possibilities.out.txt", "w") as f:
+with open("valid_possibilities.out.txt", "w") as f:  # Write BIP-39 valid mnemonics to JSON for checker.py
     f.write(json.dumps(valid_mnemonics))
 
-print(f'Found {len(valid_mnemonics)} valid mnemonics.')
+print(f'\nFound {len(valid_mnemonics)} valid mnemonics.')
